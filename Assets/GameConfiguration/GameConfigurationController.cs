4c3dee673468d3f8d6e911328e3339160dee8c96
Christian Farmer <christian.gt.farmer@gmail.com>
2023-12-19 12:02:02 -0600

    Add medium difficulty, update difficulty configs

--- Assets/GameConfiguration/GameConfigurationController.cs
+++ Assets/GameConfiguration/GameConfigurationController.cs
@@ -28,13 +28,24 @@ public class GameConfigurationController: MonoBehaviour {
     this.enemy.stopProbability = 0.3f;
   }
 
+  public void SetMediumConfiguration() {
+    print($"[GameConfigurationController#SetMediumConfiguration]");
+
+    this.enemySpawner.quantity = 25;
+    this.counter.count = 0;
+    this.counter.maxCount = 25;
+    this.enemy.maxVelocity = 10;
+    this.enemy.maxDirectionDuration = 5;
+    this.enemy.stopProbability = 0.2f;
+  }
+
   public void SetHardConfiguration() {
     print($"[GameConfigurationController#SetHardConfiguration]");
 
-    this.enemySpawner.quantity = 10;
+    this.enemySpawner.quantity = 50;
     this.counter.count = 0;
-    this.counter.maxCount = 10;
-    this.enemy.maxVelocity = 20;
+    this.counter.maxCount = 50;
+    this.enemy.maxVelocity = 30;
     this.enemy.maxDirectionDuration = 5;
     this.enemy.stopProbability = 0.1f;
   }
@@ -42,11 +53,11 @@ public class GameConfigurationController: MonoBehaviour {
   public void SetImpossibleConfiguration() {
     print($"[GameConfigurationController#SetImpossibleConfiguration]");
 
-    this.enemySpawner.quantity = 10;
+    this.enemySpawner.quantity = 100;
     this.counter.count = 0;
-    this.counter.maxCount = 10;
-    this.enemy.maxVelocity = 50;
-    this.enemy.maxDirectionDuration = 5;
-    this.enemy.stopProbability = 0.05f;
+    this.counter.maxCount = 100;
+    this.enemy.maxVelocity = 100;
+    this.enemy.maxDirectionDuration = 1;
+    this.enemy.stopProbability = 0.01f;
   }
 }
